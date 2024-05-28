663e34bdbe6a0079b11c07ec904cbbf2237cc97e
Jiuseppe STIS Bolivia <joseluisfloresquisbert@gmail.com>
2024-05-27 22:00:31 -0400

    desarrollo xlsx

--- api/controllers/ReporteXLSXController.php
+++ api/controllers/ReporteXLSXController.php
@@ -25,22 +25,27 @@ class ReporteXLSXController {
             // Obteniendo los datos del archivo en Excel
             $temporal = $file['file']['tmp_name'];
             $name = $file['file']['name'];
-            $xlsx = new ReporteXLSX($name, $temporal, 2, ['index','codigo','paterno','materno','nombre','estado','moneda','aporte','observacion']);
+            $xlsx = new ReporteXLSX($name, $temporal, 2, ['index','codigo','paterno','materno','nombre','estado','moneda','monto','observacion']);
             $registros = $xlsx->load();
+            $inserts = array();
             // Verificando la existencia del codigo de usuario (numero tin) en la BD
             foreach($registros as $key => $registro){
                 //print_r($registro);
-                $lista = array_filter($socios, fn($socio) => $socio['nro_tin']==$registro['codigo']);
+                $lista = array_filter($socios, fn($socio) => $socio['nro_tin'] == $registro['codigo']);
 
                 if(count($lista) > 0){
-                  echo "SIOIIIII";
+                  print_r($lista);
+                  /*array_push($inserts, array(
+                    'idSocio' => $lista[0]['idSocio'],
+                    'monto' => $registro['monto'],
+                    'observacion' => $registro['observacion'],
+                  ));*/
+                  echo "asdasdasd\n";
                 }else{
                   echo "Socio no encontrado: ".$registro['codigo']."<br>";
                 }
-                print_r($key);
-                print_r($lista);
-                print_r("<br>");
             }
+            print_r($inserts);
         }else{
           Response::error_json(['message' => "Mes y gestion registrados con anterioridad."], 200);
         }
